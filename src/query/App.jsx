7146bcdc42882a333bb7c55545a90ca548f05e30
LeeBoo1995 <731420476@qq.com>
2020-02-08 21:45:44 +0800

    完成query模块数据结构搭建

--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,16 +1,25 @@
+import React from 'react'
 import { connect } from 'react-redux';
-
-
 import './App.css'
 
+import Nav from '../common/Nav'
+import List from './List'
+import Bottom from './Bottom'
 
 function App(props) {
 
+    return (
+        <div>
+            <Nav></Nav>
+            <List></List>
+            <Bottom></Bottom>
+        </div>
+    )
 
 
 }
 
 export default connect(
     function mapStateToProps(state) { },
-    function mapDispatchToProps(dispatch){}
+    function mapDispatchToProps(dispatch) { }
 )(App);
